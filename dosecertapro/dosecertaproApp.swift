9cfdf57fab3cdc1482fcad587114ce41d96c8998
João Elias <joao.candias@outlook.com>
2026-03-14 10:29:52 -0300

    Initial Commit

---
+++ dosecertapro/dosecertaproApp.swift
@@ -0,0 +1,17 @@
+//
+//  dosecertaproApp.swift
+//  dosecertapro
+//
+//  Created by João Elias Cândido Reis on 14/03/26.
+//
+
+import SwiftUI
+
+@main
+struct dosecertaproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
